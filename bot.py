b7e4c03ca16307170fffb031c7a86a32c7fc5bdc
wattanai sathuphan <wattanai.sat@ku.th>
2019-09-27 19:48:07 +0700

    got a box

--- bot.py
+++ bot.py
@@ -3,6 +3,20 @@ import time
 import pyautogui
 import numpy as np
 
+def fill(img, x, y, c):
+    if(x<0 or y<0):
+        return False
+    if(x==img.shape[1] or y==img.shape[0]):
+        return False
+    if(img[y,x] != c and img[y,x]>250):
+        img[y,x] = c
+        fill(img, x+1, y, c)
+        # fill(img, x-1, y, c)
+        fill(img, x, y+1, c)
+        fill(img, x, y-1, c)
+
+
+
 time.sleep(1)
 font = cv2.FONT_HERSHEY_COMPLEX
 #img = pyautogui.screenshot('sceenshot.png')
@@ -10,17 +24,12 @@ img = cv2.imread('sceenshot.png', cv2.IMREAD_GRAYSCALE)
 img = cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
 ret, thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 thresh[100,201] = 255
-for i in range(img.shape[0]-3):
-    for j in range(img.shape[1]-3):
-        count = 0
-        for mi in range(3):
-            for mj in range(3):
-                if(thresh[mi+i,mj+j]==0):
-                    count += 1
-        if(count > 7):
-            for mi in range(3):
-                for mj in range(3):
-                    thresh[mi+i,mj+j] = 128
+x = 0
+y = int(img.shape[0]/2)
+while(thresh[y,x] < 128):
+    x += 1
+print(x,y)
+fill(thresh, x, y, 128)
 
 print("done")
 
