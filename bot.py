66d3232e30350d2845e513604098b7638583d13a
wattanai sathuphan <wattanai.sat@ku.th>
2019-09-30 04:03:55 +0700

    done recognition

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 
 sys.setrecursionlimit(100000)
-num_size = 64
+num_size = 28
 def fill(img, x, y, c):
     if(x<0 or y<0):
         return False
@@ -35,17 +35,46 @@ def crop_it_rev(img):
 def load_number_img():
     for i in range(1, 10):
         num.append(cv2.imread(f'{i}.png', cv2.IMREAD_GRAYSCALE))
-        print(num[i].shape)
-        num[i] = cv2.resize(num[i], (num_size, num_size))
+        #print(num[i].shape)
         num[i] = cv2.threshold(num[i], 128, 255, cv2.THRESH_BINARY)[1]
+        minx = num[i].shape[1]
+        maxx = 0
+        miny = num[i].shape[0]
+        maxy = 0
+        for j in range(num[i].shape[0]):
+            for k in range(num[i].shape[1]):
+                if(num[i][j, k] < 20):
+                    minx = min(minx, k)
+                    maxx = max(maxx, k)
+                    miny = min(miny, j)
+                    maxy = max(maxy, j)
+        num[i] = num[i][miny:maxy, minx:maxx]
+        num[i] = cv2.resize(num[i], (num_size, num_size))
+        #print(minx, miny)
+        #print(maxx, maxy)
+        cv2.imshow(f'{i}', num[i])
 
-def check_num(img, x, y):
+def check_num(full_img, x, y):
+    img = full_img[y:y+60, x:x+60]
     match = [0 for i in range(10)]
+    minx = img.shape[1]
+    maxx = 0
+    miny = img.shape[0]
+    maxy = 0
+    for j in range(img.shape[0]):
+        for k in range(img.shape[1]):
+            if(img[j, k] < 20):
+                minx = min(minx, k)
+                maxx = max(maxx, k)
+                miny = min(miny, j)
+                maxy = max(maxy, j)
+    test_img = img[miny:maxy, minx:maxx]
     for i in range(1, 10):
-        print(i, )
-        for xx in range(num_size):
-            for yy in range(num_size):
-                if(abs(num[i][yy][xx] - img[y+yy][x+xx])<10):
+        img = test_img[:, :]
+        img = cv2.resize(img, (num[i].shape[1], num[i].shape[0]))
+        for xx in range(num[i].shape[1]):
+            for yy in range(num[i].shape[0]):
+                if(abs(num[i][yy][xx] - img[yy][xx])<10):
                     match[i]+=1
                     # print(1,end='')
                 else:
@@ -53,6 +82,7 @@ def check_num(img, x, y):
                     pass
             # print()
     print(match)
+    cv2.imshow("test", img)
 
 time.sleep(1)
 #img = pyautogui.screenshot('sceenshot.png')
@@ -70,7 +100,7 @@ thresh = crop_it(thresh)
 thresh = crop_it_rev(thresh)
 print(thresh.shape)
 #(424, 320)
-check_num(thresh, 14+64+2, 156+128+4)
+check_num(thresh, 14+64+8, 156+128+8)
 print("done")
 
 cv2.imshow('display', thresh)
