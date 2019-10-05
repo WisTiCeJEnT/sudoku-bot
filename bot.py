c2c2a4df0cf58dcda3c1bbcccac52aad6449f189
wattanai sathuphan <wattanai.sat@ku.th>
2019-10-06 03:27:38 +0700

    no recursive

--- bot.py
+++ bot.py
@@ -2,10 +2,8 @@ import cv2
 import time
 import pyautogui
 import numpy as np
-import sys
 import os
 
-sys.setrecursionlimit(100000)
 num_size = 28
 
 def sudoku_solver(problem_tbl):
@@ -48,17 +46,36 @@ def fill_the_table(img):
         pos_tbl.append(tmp2)
     return (tbl, pos_tbl)
 
-def fill(img, x, y, new_color, old_color):
+def visit_able(img, x, y, color):
     if(x<0 or y<0):
         return False
     if(x==img.shape[1] or y==img.shape[0]):
         return False
-    if(img[y,x] != new_color and abs(img[y,x]-old_color)<20):
-        img[y,x] = new_color
-        fill(img, x+1, y, new_color, old_color)
-        # fill(img, x-1, y, c)
-        fill(img, x, y+1, new_color, old_color)
-        fill(img, x, y-1, new_color, old_color)
+    if(img[y][x] == color):
+        return False
+    return True
+
+def fill(img, starter_x, starter_y, new_color):
+    old_color = img[starter_y, starter_x]
+    queue = []
+    queue.append({'x' : starter_x, 'y' : starter_y})
+    while(len(queue) > 0):
+        x = queue[0]['x']
+        y = queue[0]['y']
+        queue = queue[1:]
+        if(not visit_able(img, x, y, new_color)):
+            continue
+        if(abs(int(img[y,x])-int(old_color))<20):
+            img[y,x] = new_color
+            if(visit_able(img, x+1, y, new_color)):
+                queue.append({'x' : x+1, 'y' : y})
+            if(visit_able(img, x-1, y, new_color)):
+                queue.append({'x' : x-1, 'y' : y})
+            if(visit_able(img, x, y+1, new_color)):
+                queue.append({'x' : x, 'y' : y+1})
+            if(visit_able(img, x, y-1, new_color)):
+                queue.append({'x' : x, 'y' : y-1})
+    return img
 
 def crop_it(img):
     for i in range(img.shape[0]):
@@ -77,7 +94,6 @@ def crop_it_rev(img):
 def load_number_img():
     for i in range(10):
         num.append(cv2.imread(f'{i}.png', cv2.IMREAD_GRAYSCALE))
-        #print(num[i].shape)
         num[i] = cv2.threshold(num[i], 128, 255, cv2.THRESH_BINARY)[1]
         if(i == 0):
             num[i] = cv2.resize(num[i], (num_size, num_size))
@@ -95,9 +111,6 @@ def load_number_img():
                     maxy = max(maxy, j)
         num[i] = num[i][miny:maxy, minx:maxx]
         num[i] = cv2.resize(num[i], (num_size, num_size))
-        #print(minx, miny)
-        #print(maxx, maxy)
-        #cv2.imshow(f'{i}', num[i])
 
 def check_num(full_img, x, y):
     img = full_img[y:y+60, x:x+60]
@@ -147,6 +160,8 @@ def print_tbl(tbl):
         for j in range(9):
             print(tbl[i][j], end=' ')
         print()
+
+#====================================   Main()   =====================================
     
 print("starting")
 time.sleep(1)
@@ -164,24 +179,20 @@ x = 0
 y = int(img.shape[0]/2)
 while(thresh[y,x] < 128):
     x += 1
-fill(thresh, x, y, 128, 255)
+fill(thresh, x, y, 128)
 cut_point_y = thresh.shape[0]
 cut_point_x = thresh.shape[1]
-print(cut_point_x, " ", cut_point_y)
 thresh = crop_it(thresh)
 cut_point_y -= thresh.shape[0]
 cut_point_x -= thresh.shape[1]
 thresh = crop_it_rev(thresh)
-print(cut_point_x, " ", cut_point_y)
-tmp = img[850:-200, 389:1000]
-
-#cv2.imshow('img', img[-400: , :])
+print("cut point =", cut_point_x, cut_point_y)
 print(thresh.shape)
 x = 0
 y = int(thresh.shape[0]/2)
 while(thresh[y,x] > 20):
     x += 1
-fill(thresh, x, y, 255, 0)
+fill(thresh, x, y, 255)
 problem_tbl, position_tbl = fill_the_table(thresh)
 print("Question . . .")
 print_tbl(problem_tbl)
@@ -189,10 +200,8 @@ answer_tbl = sudoku_solver(problem_tbl)
 print("Answer . . .")
 print_tbl(answer_tbl)
 auto_answer(answer_tbl, cut_point_x, cut_point_y, position_tbl, 1, img)
-#print(answer_tbl)
 
 print("done")
 #cv2.imshow("GG", img[:500 ,:])
 #cv2.waitKey()
-#time.sleep(5)
 cv2.destroyAllWindows()
